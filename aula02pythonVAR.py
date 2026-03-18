d54b204733854f0c309de34f282982787e36aacc
ojuauml <joaolukkas092006@gmail.com>
2026-03-18 20:50:31 -0300

    python aula 2 | FIAP

---
+++ aula02pythonVAR.py
@@ -0,0 +1,12 @@
+nome = input("Digite seu nome: ")
+print("Seja bem-vindo", nome)
+
+dia = input("\nDia de nascimento: ")
+mes = input("mes de nascimento: ")
+ano = input("Ano de nascimento: ")
+print(dia,"/", mes, "/", ano)
+
+
+num1 = float(input("\nDigite um valor: "))
+num2 = float(input("Digite outro valor: "))
+print("\nResultado = " , num1 + num2)
